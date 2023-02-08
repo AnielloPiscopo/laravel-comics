6df23d60e1460413cb97bf703e9a415c99dddb15
AnielloPiscopo <aniello.piscopo0707@gmail.com>
2023-02-08 18:35:14 +0100

    Completed the layout

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -7,7 +7,16 @@
     <h2 class="title text-uppercase p-absolute">Current Series</h2>
 
     <div class="comics-container d-flex wrap">
-        
+        @foreach ($comics as $comic)
+        <article class="card">
+            <a href="#">
+                <div class="img-container">
+                    <img src="{{$comic['thumb']}}" alt="{{$comic['series']}}" title="{{$comic['series']}}">
+                </div>
+                <span class="text-uppercase">{{ $comic['series'] }}</span>
+            </a>
+        </article>
+        @endforeach
     </div>
 
     <div class="btn-container d-flex justify-center">
